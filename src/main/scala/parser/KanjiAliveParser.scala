6949e8e6dc57ff47678dd2d7cfc189b9c32bcbc3
Danilo Salvio <danilo_salvio@live.com>
2018-03-23 23:49:39 -0300

    Drafts KanjiAlive parser.

---
+++ src/main/scala/parser/KanjiAliveParser.scala
@@ -0,0 +1,15 @@
+package parser
+
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import parser.Hello.spark
+
+object KanjiAliveParser {
+  def parseKanjiAlive(path: String)(implicit spark: SparkSession): DataFrame = {
+    spark.read.json(ScalaConfig.KanjiAliveP).withColumnRenamed("kanji", "kaKanji")
+      .withColumnRenamed("kmeaning", "kaMeanings")
+      .withColumnRenamed("onyomi", "kaOnYomi")
+      .withColumnRenamed("kunyomi", "kaKunYomi")
+      .withColumnRenamed("onyomi_ja", "kaOnYomi_ja")
+      .withColumnRenamed("kunyomi_ja", "kaKunYomi_ja")
+  }
+}
