14e895156f36e78359978a837d16f03b14255449
Danilo Salvio <danilo_salvio@live.com>
2018-03-23 23:20:50 -0300

    Refactors EDICT.

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -84,31 +84,9 @@ object Hello {
     val toDbl = udf[Double, String](_.toDouble)
 
       // EDICT PARSER START
-      val parseEdict = udf { (r: String) =>
-        val parts = r.split('/').map(_.trim)
-        val word = parts.head.split('[').head.trim
-        word
-      }
-
-    val parseEdictTs = udf { (r: String) =>
-      val parts = r.split('/').map(_.trim)
-      val translations = parts.tail.filterNot(s => s == null || s.isEmpty).map { s =>
-        val shitInParens: Array[String] = Option(StringUtils.substringsBetween(s, "(", ")")).getOrElse(Array[String]()).filterNot((j: String) => j == null || j.isEmpty())
-        val numsInParens = shitInParens.map(_.trim).filter(StringUtils.isNumeric)
-
-        def removeUnwanted(seqs: Seq[String], from: String) = seqs.foldLeft(from)((z, i) => StringUtils.remove(z, i))
-
-        (removeUnwanted(shitInParens.map(i => "(" + i + ")"), s).trim)
-      }.filterNot(s => s == null || s.isEmpty).toSeq
-      translations
-    }
-
-    val edict = spark.read.text(ScalaConfig.Edict)
-      .withColumn("edictWord", parseEdict('value))
-      .withColumn("translations", parseEdictTs('value))
-      .dropDuplicates("edictWord") //should better join the duplicate data together but we
-      .drop('value)
-
+      //    val edict = EdictParser.parseEdict(ScalaConfig.Edict)
+      val edict = LocalCache.of(ScalaConfig.Edict, EdictParser.parseEdict(ScalaConfig.Edict), true)
+      edict.show(50)
       // EDICT PARSER END
 
     edict.show(200, false)
@@ -402,13 +380,6 @@ object Hello {
   }
 
     //START REFACTORING CODE
-    // EDICT Refactoring
-//    val edict = EdictParser.parseEdict(ScalaConfig.Edict)
-    val edict = LocalCache.of(ScalaConfig.Edict, EdictParser.parseEdict(ScalaConfig.Edict), true)
-    edict.show(50)
-    println(edict.count)
-    // END EDICT Refactoring
-
     // Kanji Freq Refactoring
     val kanjiFreqs = FreqParser.parseAll(ScalaConfig.aoFreq, ScalaConfig.twitterFreq, ScalaConfig.wikipediaFreq, ScalaConfig.newsFreq, ScalaConfig.allFreqs)
     kanjiFreqs.show(52)
