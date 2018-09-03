d0a017dd20c1fc198e6fac3cf381227c5764a651
Danilo Salvio <danilo_salvio@live.com>
2018-09-03 02:17:27 -0300

    Modifies script to take in params and also adds sleep to avoid db throattleling.

--- writeToDynamo.sh
+++ writeToDynamo.sh
@@ -1,7 +1,10 @@
 #!/nix/store/pw59d08i9fyn9rj6w3yqk0gkc54i5ccz-system-path/bin/bash
 #!/bin/bash
 
-declare tn="vocab-test-table"
+declare tn=$1
+# declare tn="vocab-test-table"
+declare directory=$2
+# declare directory="./models"
 
 # declare header='{"'${tn}'": ['
 declare header='{"RequestItems":{"'${tn}'": ['
@@ -9,8 +12,9 @@ declare header='{"RequestItems":{"'${tn}'": ['
 # declare footer=']}'
 declare footer=']}}'
 
-for filename in ./models/*; do
+for filename in $directory/*; do
   while mapfile -t -n 10 ary && ((${#ary[@]})); do
+      sleep 0.5
       content="${ary[@]}"
       upload="$header"${content::-1}"$footer"
       aws dynamodb batch-write-item --region us-east-1 --cli-input-json "$upload"
