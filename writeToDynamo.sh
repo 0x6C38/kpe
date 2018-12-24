86073978cbb6f6d22ad0330bad9bdcf05cee15f9
Danilo Salvio <danilo_salvio@live.com>
2018-12-23 23:17:33 -0300

    Updates the script to include a counter and keep track of the time elapsed.

--- writeToDynamo.sh
+++ writeToDynamo.sh
@@ -1,10 +1,10 @@
-#!/nix/store/pw59d08i9fyn9rj6w3yqk0gkc54i5ccz-system-path/bin/bash
 #!/bin/bash
+# #!/nix/store/pw59d08i9fyn9rj6w3yqk0gkc54i5ccz-system-path/bin/bash
 
 declare tn=$1
-# declare tn="vocab-test-table"
+# declare tn="kanjiapp-dev-master"
 declare directory=$2
-# declare directory="./models"
+# declare directory="./output-aws-word-entries"
 declare x=$3
 # declare x=10
 
@@ -14,6 +14,8 @@ declare header='{"RequestItems":{"'${tn}'": ['
 # declare footer=']}'
 declare footer=']}}'
 
+SECONDS=0
+
 for filename in $directory/*; do
   while mapfile -t -n $x ary && ((${#ary[@]})); do
       sleep 0.5
@@ -21,5 +23,14 @@ for filename in $directory/*; do
       upload="$header"${content::-1}"$footer"
       aws dynamodb batch-write-item --region us-east-1 --cli-input-json "$upload"
       #echo $upload
+
+      counter=$((counter+x))
+      printf "Processed: ${counter}"
+      echo ""
   done < $filename
 done
+
+duration=$SECONDS
+echo "$(($duration / 60 / 60)) hours $(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
+
+
